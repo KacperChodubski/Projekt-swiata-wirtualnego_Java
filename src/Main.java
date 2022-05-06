f6985973c556ac2b1cc47fe86e28ffc97834070c
Kacper-stacjonarka <kaper02@o2.pl>
2022-05-06 15:05:07 +0200

    dodanie okna

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.awt.*;
+
+public class Main
+{
+    public static Dimension MAIN_WINDOW_DIMENSION = new Dimension(800, 600);
+
+    public static void main(String[] args)
+    {
+//        MainWindow mainWindow = new MainWindow(MAIN_WINDOW_DIMENSION);
     }
 }
